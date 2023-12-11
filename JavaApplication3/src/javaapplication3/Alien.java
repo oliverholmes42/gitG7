6311cd44a03a83bd380569860009394aee4d9df8
aiham <aiham@10.22.20.4>
2023-12-11 12:06:39 +0100

    fixed the class storing data about alien(s).

--- JavaApplication3/src/javaapplication3/Alien.java
+++ JavaApplication3/src/javaapplication3/Alien.java
@@ -4,64 +4,139 @@
  */
 package javaapplication3;
 
-import java.util.HashMap;
+import java.time.LocalDate;
 
 /**
  *
- * @author vilso
+ * @author aiham
  */
 public class Alien {
-    private String alien_name;
     private int alien_ID;
-    private String alien_gender;
-    private String alien_race;
-    private HashMap<Double, Double> alien_location;
+    private LocalDate Registrationdate;
+    private String alien_epost;
+    private String alien_password;
+    private String alien_name;
+    private String alien_phonenumber;
+    private Location location;
+    private Agent responsible_agent;
 
-    public Alien(String alien_name, int alien_ID, String alien_gender, String alien_race){
+
+
+    public Alien(int alien_ID, LocalDate Registrationdate, String alien_epost, 
+            String alien_password, String alien_name, String alien_phonenumber, 
+            Location location, Agent responsible_agent){
+        
+        this.alien_ID = alien_ID;
+        this.Registrationdate = Registrationdate;
+        this.alien_epost = alien_epost;
+        this.alien_password = alien_password;
         this.alien_name = alien_name;
-        this.alien_ID = alien_ID;
-        this.alien_gender = alien_gender;
-        this.alien_race = alien_race;
-        this.alien_location = new HashMap<>();
-    }
+        this.alien_phonenumber = alien_phonenumber;
+        this.location = location;
+        this.responsible_agent = responsible_agent;
+        
+        }
+
+// Setter for name
     
-    public void setName(String alien_name){
-       this.alien_name = alien_name;
-    }
-    
-    public String getName(){
-    return alien_name;
-    }
-    
-    public void setGender(String alien_gender){
-    this.alien_gender = alien_gender;
-    }
-    
-    public String getGender(){
-    return alien_gender;
-    }
-    
-    public void setID(int Alien_ID){
+    public void setalienID(int alien_ID) {
         this.alien_ID = alien_ID;
     }
     
-    public int getAlienID(){
-    return alien_ID;
+// Getter for name
+    
+    public int getalienID() {
+        return alien_ID;
     }
     
-    public void setRace(String alien_race){
-    this.alien_race = alien_race;
+// Setter for Registrationdate
+    
+    public void setRegistrationDate(LocalDate Registrationdate) {
+        this.Registrationdate = Registrationdate;
     }
     
-    public String getRace(){
-    return alien_race;
+// Getter for Registrationdate
+
+    public LocalDate getRegistrationDate() {
+        return Registrationdate;
     }
     
-    public void setLocation(double x_coordinates, double y_coordinates){
-        this.alien_location = alien_location;
+    // Setter for alienEpost
+    
+    public void setalienEpost(String alien_epost) {
+        this.alien_epost = alien_epost;
     }
     
-    public HashMap<Double, Double> getLocation(){
-        return alien_location;
+// Setter for alienEpost
+    
+    public String getalienEpost() {
+        return alien_epost;
     }
+    
+// Setter for alienPassword
+    
+    public void setalienPassword(String alien_password) {
+        this.alien_password = alien_password;
+    }
+    
+// Getter for alienPassword
+
+    public String getalienPassword() {
+        return alien_password;
+    }
+    
+// Setter for alienName
+    
+    public void setalienName(String alien_name) {
+        this.alien_name = alien_name;
+    }
+    
+// Getter for alienName
+
+    public String getalienName() {
+        return alien_name;
+    }
+    
+    
+// Setter for alienPhonenumber
+
+    public void setalienPhonenumber(String alien_phonenumber) {
+        this.alien_phonenumber = alien_phonenumber;
+    }
+  
+// Getter for alienPhonenumber
+
+    public String getalienPhonenumber() {
+        return alien_phonenumber;
+    }
+
+// Setter for alienLocation
+
+    public void setalienLocation(Location location) {
+        this.location =  location;
+    }
+    
+// Getter for alienLocation
+
+    public Location getalienLocation() {
+        return location;
+    }
+    
+// Setter for agent responsible for alien
+
+    
+    public void setalienResponslbleAgent(Agent responsible_agent) {
+        this.responsible_agent =  responsible_agent;
+    }
+    
+// Getter for agent responsible for alien
+
+    public Agent getresponsible_agent() {
+        return responsible_agent;
+    }
+    
+    
 }
+        
+        
+  
